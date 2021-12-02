aa0347c845ce9e7b8202eda253440c111f5410cb
jadugamja <87695983+jadugamja@users.noreply.github.com>
2021-12-03 04:18:41 +0900

    Update KosmosNoticeController.java
    공지사항 목록 조회 구현. 
    1. 코스모스 고등학교 홈.jsp 페이지에서 <a href="noticeList.k"></a>로 url 매핑함.
    2. KosmosNoticeController에서 "noticeList.k"로 매핑된 함수 noticeList(nvo, model)를 찾음.
    3. KosmosNoticeService의 noticeList() 함수에 nvo 입력하고, 이를 변수 listAll로 받음.
    4. listAll.size()는 nvo 리스트의 숫자(= 곧 게시글 수)를 알려줌.
    5. 게시글 수가 1개 이상이면, 모델 객체에 listAll 값을 넣어 뷰로 보냄.
    6. 함수 처리가 끝난후 noticeList.jsp 페이지 보여줌.

--- src/com/kosmos/notice/controller/KosmosNoticeController.java
+++ src/com/kosmos/notice/controller/KosmosNoticeController.java
@@ -1,5 +1,68 @@
 package com.kosmos.notice.controller;
 
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.kosmos.notice.service.KosmosNoticeService;
+import com.kosmos.notice.vo.KosmosNoticeVO;
+import com.kosmos.common.file.FileUploadUtil;
+
+
+@Controller
 public class KosmosNoticeController {
+
+	Logger logger = Logger.getLogger(KosmosNoticeController.class);
+	
+	@Autowired(required=false)
+	private KosmosNoticeService kosmosNoticeService;
+	
+	public KosmosNoticeController(KosmosNoticeService kosmosNoticeService) {
+		this.kosmosNoticeService = kosmosNoticeService;
+	}
+	
+	// ******************************************************************** /
+	// 공지사항 글쓰기 폼 출력
+  @GetMapping("noticeForm")
+	public String noticeForm(KosmosNoticeVO nvo) {
+		logger.info("noticeForm() 호출");
+		return "notice/noticeForm";
+	}
+	// ******************************************************************** /
+
+	
+	// ******************************************************************** /
+	// 공지사항 목록 조회
+  @GetMapping("noticeList")
+	public String noticeList(KosmosNoticeVO nvo, Model model) throws Exception {
+		logger.info("noticeList() 호출");
+		
+		List<KosmosNoticeVO> listAll = kosmosNoticeService.noticeList(nvo);
+		logger.info("Controller.listAll.size(): " + listAll.size());
+		
+		if(listAll.size() > 0) {
+			// 게시글 숫자 셈
+//			nvo.setTotalCount(listAll.get(0).getTotalCount());
+			model.addAttribute("listAll", listAll);
+		}
+		
+		return "notice/noticeList";
+
+	}
+	// ******************************************************************** /	
 
 }
