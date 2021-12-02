c3ddcc00c753d98f739bb09afbd79850f9ec0aa9
jadugamja <87695983+jadugamja@users.noreply.github.com>
2021-12-03 04:20:01 +0900

    Update KosmosNoticeDAO.java

--- src/com/kosmos/notice/dao/KosmosNoticeDAO.java
+++ src/com/kosmos/notice/dao/KosmosNoticeDAO.java
@@ -1,5 +1,18 @@
 package com.kosmos.notice.dao;
 
+import java.util.List;
+
+import com.kosmos.notice.vo.KosmosNoticeVO;
+
 public interface KosmosNoticeDAO {
+
+	// 게시글 작성
+	public int noticeInsert(KosmosNoticeVO nvo);
+	// 게시글 목록 조회
+	public List<KosmosNoticeVO> noticeList(KosmosNoticeVO nvo);
+	// 게시글 상세 조회
+//	public List<KosmosNoticeVO> noticeDetail(KosmosNoticeVO nvo);
+	public int noticeUpdate(KosmosNoticeVO nvo);
+	public int noticeDelete(KosmosNoticeVO nvo);
 
 }
